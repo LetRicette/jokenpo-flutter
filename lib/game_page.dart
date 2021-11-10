cfdbfb461977f4ec75f2dc62bbe7b396317cb982
LetRicette <leticiaricette@id.uff.br>
2021-11-10 11:39:24 -0300

    first commit

---
+++ lib/game_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class GamePage extends StatefulWidget {
+  GamePage({Key? key}) : super(key: key);
+
+  @override
+  _GamePageState createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('JoKenPo'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
